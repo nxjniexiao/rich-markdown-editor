14afb50fc5c432a592f8ad1ba608b08d0d59a43b
nxjniexiao <30365414+nxjniexiao@users.noreply.github.com>
2023-02-10 16:15:10 +0800

    test: 把index.stories中的所有用例合并成一个用例

---
+++ src/stories/all.stories.tsx
@@ -0,0 +1,36 @@
+import Editor from "./index";
+import { Props } from "..";
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import * as stories from "./index.stories";
+
+export default {
+  title: "Editor",
+  component: Editor,
+  argTypes: {
+    value: { control: "text" },
+    readOnly: { control: "boolean" },
+    onSave: { action: "save" },
+    onCancel: { action: "cancel" },
+    onClickHashtag: { action: "hashtag clicked" },
+    onClickLink: { action: "link clicked" },
+    onHoverLink: { action: "link hovered" },
+    onShowToast: { action: "toast" },
+    onFocus: { action: "focused" },
+    onBlur: { action: "blurred" },
+    disableExtensions: { control: "array" },
+  },
+  args: {
+    disableExtensions: [],
+  },
+} as Meta;
+
+const Template: Story<Props> = args => <Editor {...args} />;
+
+export const All = Template.bind({});
+All.args = { defaultValue: "" };
+All.args.defaultValue = Object.keys(stories).reduce((prev, name) => {
+  if (name === "default") return prev;
+  // eslint-disable-next-line import/namespace
+  return (prev += stories[name].args.defaultValue + "\n");
+}, "");
