354a6a0f644ad1cb081e9d4fd2dc18318e1f7d3e
nxjniexiao <30365414+nxjniexiao@users.noreply.github.com>
2023-08-23 17:54:13 +0800

    fix: fix error of parseDOM and toDOM

--- src/stories/external-extensions/attachment/attachment-node.tsx
+++ src/stories/external-extensions/attachment/attachment-node.tsx
@@ -24,7 +24,7 @@ export default class Attachment extends Node {
       },
       parseDOM: [
         {
-          tag: "attachment",
+          tag: `[class="${this.name}"`,
           getAttrs: (dom: HTMLIFrameElement) => {
             const type = dom.getAttribute("data-type") || "";
             const id = dom.getAttribute("data-id") || "";
@@ -33,8 +33,12 @@ export default class Attachment extends Node {
         },
       ],
       toDOM: node => [
-        "attachment",
-        { "data-type": node.attrs.type, "data-id": node.attrs.id },
+        "span",
+        {
+          class: this.name,
+          "data-type": node.attrs.type,
+          "data-id": node.attrs.id,
+        },
         0,
       ],
     };
